17de43923861a76af5e9a64a95ce21dfdc263e8a
Lebin Mathew Alex <lebinma@live.com>
2016-12-30 10:51:18 +0530

    use seperate queue to hold tracks

--- OS/disk.c
+++ OS/disk.c
@@ -1,36 +1,36 @@
 //DISK SCHEDULING
-//Alpha Version
+/* Copyright (C) 2016, Lebin Mathew Alex. All rights reserved.*/
 
 #include<stdio.h>
 
 enum Mode {FCFS, SSTF, SCAN} mode;
 enum Direction {left, right} dir;
 
-int schedule(int[], int, int, enum Mode);
+int schedule(int[], int, int[], int, enum Mode);
 void swap(int *, int *);
 int mod(int);	//mod of a value
 void drawGraph(int[], int, int, enum Mode);
 
 void main()
 {
-	int i, n, index, rp, seek, bound;
+	int i, n, queueSize, index, rp, bound, queue[100];
 	
 	do
 	{
 		printf("\nEnter the number of tracks : ");
 		scanf("%d", &n);
 		
-		int list[n+2]; //+2 is for boundaries in SCAN
+		int list[n]; 
 		
 		printf("\nEnter the tracks : \n");
 		
-		for (i=1; i<=n; i++)
+		for (i=0; i<n; i++)
 		{
 			scanf("%d", &list[i]);
 		}
 		
 		printf("\nEnter initial head position : ");
-		scanf("%d", &list[0]);
+		scanf("%d", &queue[0]);
 		
 		printf("\nEnter Boundary : ");
 		scanf("%d", &bound);
@@ -47,23 +47,17 @@ try :	printf("\nEnter Choice : ");
 			default : printf("Invalid Choice!\n"); goto try;
 		}
 		
-		seek = schedule(list, n, bound, mode);
+		queueSize = schedule(list, n, queue, bound, mode);
 		
 		printf("\nJobs are scheduled in the order :\n");
 		
-		for (i=0; i<=n; i++)
+		for (i=0; i<queueSize; i++)
 		{
-			printf("%d  ", list[i]);
+			printf("%d  ", queue[i]);
 		}
-		
-		if (mode == SCAN)
-			printf("%d  %d", list[n+1], list[n+2]);
 			
 		printf("\n\n");
-		
-		drawGraph(list, n, bound, mode);
-			
-		printf("\n\nSeek time = %d\n\n", seek);
+		drawGraph(queue, queueSize, bound, mode);
 		
 		printf("Press 1 to try again!\n");
 		scanf("%d", &rp);
@@ -71,62 +65,77 @@ try :	printf("\nEnter Choice : ");
 	} while (rp == 1);	
 }
 
-int schedule(int list[], int n, int bound, enum Mode mode)
+int schedule(int list[], int n, int queue[], int bound, enum Mode mode)
 {
-	int i, j, k, min, temp[n], size=1, seek=0;
-	
-	temp[0] = list[0];
+	int i, j, min, queueSize=1; //1 since there's starting track
 	
 	if (mode == FCFS)
 	{
+		//just copy the tracks
+		for (i=0; i<n; i++)
+		{
+			if (list[i] != queue[queueSize-1]) //add only if different
+			{
+				queue[queueSize] = list[i];
+				queueSize++;
+			}
+		}
 	}
 	else if (mode == SSTF)
 	{
-		for (i=0; i<n; i++)
+		//find the min distance track
+		for (i=0; i<n-1; i++)
 		{
 			min = i+1;
 			
-			for (j=i+1; j<=n; j++)
+			for (j=i+1; j<n; j++)
 			{
 				if (mod(list[j]-list[i]) < mod(list[min]-list[i]))
 				{
 					min = j;
 				}
 			}
-			
-			swap(&list[i+1], &list[min]);
+
+			if (list[min] != queue[queueSize-1]) //add only if different
+			{
+				queue[queueSize] = list[min];
+				queueSize++;
+			}
 		}
 	}
 	else if (mode == SCAN)
 	{
-		i = list[0];
+		i = queue[0];
 		dir = left;
-		n += 2;		//+2 locations for boundaries
-		
+
 		while (i <= bound)
 		{
-			for (k=1; k<=n; k++)
+			//check if there's any track at current instance
+			for (j=0; j<n; j++)
 			{
-				if (i == list[k])
+				//add only if different
+				if (i == list[j] && list[j] != queue[queueSize-1])
 				{
-					temp[size] = i;
-					size++;
+					queue[queueSize] = list[j];
+					queueSize++;
 				}
 			}
-			
-			if (i == 0)
+
+			if (i == 0 || i == bound)	//reached left or right end
 			{
-				temp[size] = i;
-				size++;
-				dir = right;
-				i = list[0];	
+				if (i != queue[queueSize-1])	//add only if different
+				{
+					queue[queueSize] = i;
+					queueSize++;
+				}
+
+				if (i == 0)				//if left end
+				{
+					dir = right;		//switch direction
+					i = queue[0];		//go right from start track
+				}
 			}
-			else if (i == bound)
-			{
-				temp[size] = bound;
-				size++;
-			}
-			
+
 			if (dir == left)
 			{
 				i--;
@@ -136,40 +145,27 @@ int schedule(int list[], int n, int bound, enum Mode mode)
 				i++;
 			}
 		}
-		
-		for (i=1; i<=n; i++)
-		{
-			list[i] = temp[i];
-		}
-	}
-	
-	for (i=0; i<n; i++)
-	{
-		seek += mod(list[i+1]-list[i]);
-	}
 
-	return seek;
+	return queueSize;
 }
 
 void drawGraph(int list[], int n, int bound, enum Mode mode)
 {
 	int i, j, loc, temp[n];
 	
-	if (mode == SCAN)
-		n += 2;			//+2 locations for boundaries
-	
-	for (i=0; i<=n; i++)	//make a temp list
+	for (i=0; i<n; i++)	//make a temp list
 		temp[i] = list[i];
 	
-	for (i=0; i<=n; i++)	//sort the list
+	for (i=0; i<n; i++)	//sort the list
 	{
-		for (j=0; j<n; j++)
+		for (j=0; j<n-1; j++)
 		{
 			if (temp[j] > temp[j+1])
 				swap(&temp[j], &temp[j+1]);
 		}
 	}
 	
+	/*
 	if (mode == SCAN)
 	{
 		printf("\n\n0\t");
@@ -178,22 +174,31 @@ void drawGraph(int list[], int n, int bound, enum Mode mode)
 	{
 		printf("\n\n0\t%d\t", temp[0]);
 	}
-	
-	for (i=1; i<=n; i++)
+	*/
+	printf("\n\n");
+
+	if (mode != SCAN && list[0] != 0)	//SCAN has 0 already in it
 	{
-		if (temp[i] != temp[i-1])
-			printf("%d\t", temp[i]);
+		printf("0\t");
 	}
 	
-	if (mode != SCAN)
+	for (i=0; i<n; i++)		//print rest of the tracks
+	{
+		//if (temp[i] != temp[i-1])
+		printf("%d\t", temp[i]);
+	}
+	
+	if (mode != SCAN && list[n-1] != bound)	//SCAN has bound already in it
+	{
 		printf("%d", bound);
+	}
 		
 	printf("\n\n");
 	
-	for (i=0; i<=n; i++)	//level
+	for (i=0; i<n; i++)	//level
 	{
 		//find location
-		for (j=0; j<=n; j++)
+		for (j=0; j<n; j++)
 		{
 			if (list[i] == temp[j])
 			{
@@ -206,7 +211,20 @@ void drawGraph(int list[], int n, int bound, enum Mode mode)
 					if (mode == SCAN)
 						loc = j;
 					else
-						loc = j+1;
+						loc = j+1;		//+1 for left end
+					/*
+					if (mode == SCAN)
+					{
+						loc = j;
+					}
+					else
+					{
+						if (temp[0] == 0)
+							loc = j+1;		//+1 for left end
+						else	
+							loc = j;		//
+					}
+					*/
 				}
 			}
 		}
@@ -237,6 +255,3 @@ void swap(int *a, int *b)
 	*a = *b;
 	*b = temp;
 }
-
-
-
